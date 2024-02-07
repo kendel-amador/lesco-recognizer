8c81ffc71dea9e1dbe24a3789cfd6b001df5fea5
Kendel~ <kendell21am@gmail.com>
2024-02-06 22:12:42 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+def speaker(text):
+    engine = pyttsx3.init()
+    engine.setProperty('lang', 'es')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[2].id) # Selecciona la voz española
+    engine.setProperty('rate', 150) # Aumenta la velocidad
+    engine.setProperty('pitch', 50) # Aumenta el tono
+    engine.say(text)
+    engine.runAndWait()
+
+speaker("Me llamo Esteban")
